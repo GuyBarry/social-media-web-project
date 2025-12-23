87dcd6c3957a5ed5dabd600aa7aef6abb52a673b
mayanamsterdam <maayanamsterdam@gmail.com>
2025-12-23 16:25:38 +0200

    add initial test suite with Jest

---
+++ test/auth.test.js
@@ -0,0 +1,14 @@
+beforeAll((done) => {
+  done();
+});
+
+afterAll((done) => {
+  done();
+});
+
+describe("GET / ", () => {
+      test("It should respond be true", async () => {
+          const response = 200
+          expect(response).toEqual(200);
+      });
+  });
