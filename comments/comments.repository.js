6bade9d58f4d5aec81fb18db9090aecca512e9ed
mayanamsterdam <maayanamsterdam@gmail.com>
2026-01-05 22:33:17 +0200

    Add get comment by id route

--- comments/comments.repository.js
+++ comments/comments.repository.js
@@ -2,8 +2,10 @@ import { Comment } from "../entities/mongodb/comment.module.js";
 
 const getAllComments = async () => await Comment.find({});
 const getAllCommentsByPostId = async (postId) => await Comment.find({ postId });
+const getCommentById = async (id) => await Comment.findById(id);
 
 export const commentsRepository = {
   getAllComments,
   getAllCommentsByPostId,
+  getCommentById,
 };
--- comments/comments.service.js
+++ comments/comments.service.js
@@ -2,8 +2,10 @@ import { commentsRepository } from "./comments.repository.js";
 
 const getAllComments = async () => await commentsRepository.getAllComments();
 const getAllCommentsByPostId = async (postId) => await commentsRepository.getAllCommentsByPostId(postId);
+const getCommentById = async (id) => await commentsRepository.getCommentById(id);
 
 export const commentsService = {
   getAllComments,
   getAllCommentsByPostId,
+  getCommentById,
 };
