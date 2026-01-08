8bcb207aae3ffd30b8d2ec1d840b94997c6ec30c
mayanamsterdam <maayanamsterdam@gmail.com>
2026-01-08 18:40:14 +0200

    Add Swagger documentation for schemas

--- entities/dto/comment.dto.js
+++ entities/dto/comment.dto.js
@@ -1,12 +1,46 @@
 import { z } from "zod";
 import { notEmptyStringSchema } from "./zodUtils.js";
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     CreateComment:
+ *       type: object
+ *       required:
+ *         - sender
+ *         - message
+ *         - postId
+ *       properties:
+ *         sender:
+ *           type: string
+ *           required: true
+ *         message:
+ *           type: string
+ *           required: true
+ *         postId:
+ *           type: string
+ *           required: true
+ */
 export const createCommentSchema = z.strictObject({
   sender: notEmptyStringSchema("Sender "),
   message: notEmptyStringSchema("Message"),
   postId: notEmptyStringSchema("postId"),
 });
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     UpdateComment:
+ *       type: object
+ *       required:
+ *         - message
+ *       properties:
+ *         message:
+ *           type: string
+ *           required: true
+ */
 export const updateCommentSchema = z.strictObject({
   message: createCommentSchema.shape.message,
 });
