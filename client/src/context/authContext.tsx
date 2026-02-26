f855eba1506af3d2d6b52b5000b730dbf03253b8
mayanamsterdam <maayanamsterdam@gmail.com>
2026-02-26 20:29:53 +0200

    Add authentication context and user entity definitions

---
+++ client/src/context/authContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, useContext } from "react";
+import type { UserLogin } from "../auth/types/userLogin";
+import type { UserRegistration } from "../auth/types/userRegistration";
+import type { User } from "../entities/User";
+
+export interface AuthResultHandlers {
+  onSuccess?: (user: User) => void;
+  onError?: (error: Error) => void;
+}
+
+interface AuthContextValue {
+  user: User | null;
+  login: (
+    userCredentials: UserLogin,
+    authResultHandlers?: AuthResultHandlers,
+  ) => Promise<void>;
+  register: (
+    registrationData: UserRegistration,
+    authHandlers?: AuthResultHandlers,
+  ) => Promise<void>;
+  isLoadingUserAuth: boolean;
+}
+export const AuthContext = createContext<AuthContextValue>(
+  null as unknown as AuthContextValue,
+);
+
+export const useAuth = () => useContext(AuthContext);
---
+++ client/src/entities/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+  _id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  username: string;
+  email: string;
+  password: string;
+  bio?: string;
+  birthDate: Date;
+}
