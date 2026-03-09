15d2fe19e9496dd15e4c9b658f798d45c2bab416
mayanamsterdam <maayanamsterdam@gmail.com>
2026-03-09 23:33:36 +0200

    Fix import paths for attachRefreshInterceptor in API modules

--- client/src/api/commentsApi.ts
+++ client/src/api/commentsApi.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { attachRefreshInterceptor } from "./apiUtils";
+import { attachRefreshInterceptor } from "./api.utils";
 
 export const commentsApi = axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_URL}/comments`,
--- client/src/auth/api/selfAuthApi.ts
+++ client/src/auth/api/selfAuthApi.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { attachRefreshInterceptor } from "../../api/apiUtils";
+import { attachRefreshInterceptor } from "../../api/api.utils";
 
 export const selfAuthApi = axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_URL}/auth`,
