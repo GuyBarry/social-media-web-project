a07b7d44e48ee2838959ad1083930dcb87626562
mayanamsterdam <113461861+mayanamsterdam@users.noreply.github.com>
2026-03-13 16:24:40 +0200

    Apply suggestion from @mayanamsterdam

--- client/src/views/home/home.tsx
+++ client/src/views/home/home.tsx
@@ -18,14 +18,10 @@ export const HomeScreen = () => {
         {user && <ProfileCard user={user} />}
       </HomeSidebar>
       <main>
-<<<<<<< create-post
         <Box display="flex" flexDirection="column" gap="16px">
           {user && <CreatePost user={user} />}
           <FeedComponent queryResult={queryResult} />
         </Box>
-=======
-        <FeedComponent queryResult={queryResult} />
->>>>>>> main
       </main>
       <HomeSidebar>
         <Typography variant="h5" fontWeight={600}>
