ead11f6c6b5eb5cc53657fc7ebe53fe4a298fbb9
Guy Barry <guybarry5@gmail.com>
2026-03-18 16:05:49 +0200

    Remove unused function

--- client/src/api/aiApi.ts
+++ client/src/api/aiApi.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { attachRefreshInterceptor } from "./api.utils";
-import type { Mood } from "../constants/moods";
 
 export const aiApi = axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_URL}/ai`,
@@ -8,14 +7,3 @@ export const aiApi = axios.create({
 });
 
 attachRefreshInterceptor(aiApi);
-
-export const rewriteWithMood = async (
-  postContent: string,
-  mood: Mood,
-): Promise<string> => {
-  const response = await aiApi.post<{ result: string }>("/mood", {
-    postContent,
-    mood,
-  });
-  return response.data.result;
-};
