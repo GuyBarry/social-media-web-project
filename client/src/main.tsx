239fc95722ac5f8088ec89ca731185a384de1d35
mayanamsterdam <maayanamsterdam@gmail.com>
2026-03-22 10:59:19 +0200

    remove ReactQueryDevtools

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -16,8 +15,6 @@ const queryClient = new QueryClient({
   },
 });
 
-//TODO: delete ReactQueryDevtools
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
@@ -25,7 +22,6 @@ createRoot(document.getElementById("root")!).render(
         <CssBaseline />
         <App />
       </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} /> 
     </QueryClientProvider>
   </StrictMode>,
 );
