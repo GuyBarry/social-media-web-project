622b74cd747dc72b88dcfef8e8455baed8953145
mayanamsterdam <maayanamsterdam@gmail.com>
2026-03-22 11:02:26 +0200

    remove comments

--- server/middlewares/errorHandler.ts
+++ server/middlewares/errorHandler.ts
@@ -6,27 +6,16 @@ export const errorHandler: ErrorRequestHandler = (
   error: Error,
   _req: Request,
   res: Response,
-  _next: NextFunction
+  _next: NextFunction,
 ) => {
   if (error instanceof CustomException) {
     const { message, details, statusCode, stack } = error;
-
-    // console.error({
-    //   message: message,
-    //   details: details,
-    //   stack: stack,
-    // });
 
     res.status(statusCode).send({
       message,
       details,
     });
   } else {
-  // console.error({
-  //   message: error?.message,
-  //   stack: error?.stack,
-  // });
-
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
       message: "Oops, something went wrong!",
     });
