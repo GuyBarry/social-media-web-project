b5e27738cea87603dfcd489064e62a52f9350221
mayanamsterdam <maayanamsterdam@gmail.com>
2026-04-10 12:41:34 +0300

    fix: uncomment static file serving for client in app.ts

--- server/app.ts
+++ server/app.ts
@@ -33,7 +33,7 @@ export const initApp = async (): Promise<Express> => {
     }),
   );
 
-  // app.use(express.static(path.resolve(__dirname, "../client")));
+  app.use(express.static(path.resolve(__dirname, "../client")));
   app.use("/public", express.static(path.resolve(__dirname, "public")));
 
   app.use(bodyParser.urlencoded({ extended: true, limit: "1mb" }));
@@ -56,6 +56,7 @@ export const initApp = async (): Promise<Express> => {
 
   registerSwagger(app);
 
+  //SPA client routes
   const clientRoutes = [
     "/",
     "/profile",
